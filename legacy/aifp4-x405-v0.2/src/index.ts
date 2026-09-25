122960998829f7c0122d47463c69fccfdfd1576f
coinsecuritiescompany <coinsecuritiescompany@gmail.com>
2026-09-25 18:52:46 +0200

    chore(legacy): preserve src/index.ts

---
+++ legacy/aifp4-x405-v0.2/src/index.ts
@@ -0,0 +1,6 @@
+export * from "./types.js";
+export * from "./canonical.js";
+export * from "./policy-engine.js";
+export * from "./state-machine.js";
+export * from "./receipt.js";
+export * from "./orchestrator.js";
